08c9ec67dfd51dfe87a7ac52f375847feaedf042
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-20 12:49:39 -0300

    Adding an animation to the element in the shadow DOM

--- src/components/change-detection/date-display.js
+++ src/components/change-detection/date-display.js
@@ -12,10 +12,27 @@ export class DateDisplay extends LitElement {
     },
   };
 
+  //regular styles
   static styles = [changeDetectionStyles];
+
+  //animation frames
+  frames = [{ opacity: "0.5" }, { opacity: "0" }, { opacity: "1" }];
+
+  //getting element to animate from the shadow DOM
+  get datefield() {
+    return this.renderRoot?.querySelector("#datefield");
+  }
 
   render() {
     return html`<span id="datefield">${this.date?.toLocaleDateString()}</span>`;
+  }
+
+  //if date has been updated, let's add an animation to it
+  updated(changed) {
+    if (changed.has("date")) {
+      //adding an animation to the element
+      this.datefield.animate(this.frames, 1000);
+    }
   }
 }
 
